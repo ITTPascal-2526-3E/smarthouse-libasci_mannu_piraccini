f33cace32ba977006f280a1546143599d51e0659
SamueleLibasci <samuelelibasci2000@gmail.com>
2025-11-21 10:57:19 +0100

    Iniziati Lamp test

--- Test/BlaisePascal.SmartHouse.Domain.UnitTest/LampTest.cs
+++ Test/BlaisePascal.SmartHouse.Domain.UnitTest/LampTest.cs
@@ -3,9 +3,9 @@
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Lamp_TurnOnWhenLampIsOff_LampIsTurnedOn()
         {
-
+            var lampTest = new Lamp();
         }
     }
 }
