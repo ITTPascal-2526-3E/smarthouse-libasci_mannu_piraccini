4877f892327efdba9579f9371a4c81e31e4a646a
w11 <w11@PC-5107>
2025-11-24 10:28:58 +0100

    Aggiunti alcuni Test classe Lamp

--- Test/BlaisePascal.SmartHouse.Domain.UnitTest/LampTest.cs
+++ Test/BlaisePascal.SmartHouse.Domain.UnitTest/LampTest.cs
@@ -3,9 +3,38 @@
     public class UnitTest1
     {
         [Fact]
-        public void Lamp_TurnOnWhenLampIsOff_LampIsTurnedOn()
+        public void TurnOnOrOff_WhenTheLightIsOff_TurnsItOn()
         {
-            var lampTest = new Lamp(aaa);
+            // Arrange
+            var lamp = new Lamp("Test", "LED", 10, 500, true, "E27");
+
+            // Act
+            lamp.TurnOnOrOff();
+
+            // Assert
+            Assert.True(lamp.IsOn());
         }
+
+        [Fact]
+        public void TurnOnOrOff_WhenTheLightIsOn_TurnsItOff()
+        {   
+            var lamp = new Lamp("Test", "LED", 10, 500, true, "E27");
+            lamp.TurnOnOrOff(); // First turn it on
+           
+            lamp.TurnOnOrOff(); // Now turn it off
+            
+            Assert.False(lamp.IsOn());
+        }
+
+        [Fact]
+        public void DimmableControl_validValueAndDimmable_DoesNotThrow()
+        {             
+            var lamp = new Lamp("Test", "LED", 10, 500, true, "E27");
+            
+          
+        }
+
+
     }
 }
+
