db7879c2bdaee4273e8e34da597768b97b46c410
SamueleLibasci <samuelelibasci2000@gmail.com>
2026-02-10 13:33:05 +0100

    Fatte Repository Temperature Device

---
+++ src/BlaisePascal.SmartHouse.Domain/TemperatureRegolation/Repository/IAirConditionerRepository.cs
@@ -0,0 +1,20 @@
+﻿using BlaisePascal.SmartHouse.Domain.Lighting;
+using BlaisePascal.SmartHouse.Domain.TemperatureRegulation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.TemperatureRegolation.Repository
+{
+
+        internal interface IAirConditionerRepository
+        {
+            void Add(AirConditioner airConditioner);
+            void Update(AirConditioner airConditioner);
+            void Delete(AirConditioner airConditioner);
+            AirConditioner GetById(int id);
+            List<AirConditioner> GetAll();
+        }
+}
