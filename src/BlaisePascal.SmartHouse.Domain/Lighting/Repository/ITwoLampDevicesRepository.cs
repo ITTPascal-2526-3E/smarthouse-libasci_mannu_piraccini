2dae0859f59a3171e028a014b7cd0eca05ed94e2
pirazz12 <francesco.piraccini6@gmail.com>
2026-02-10 13:31:40 +0100

    ADDED Repository in TwoLampDevice and CCTV

---
+++ src/BlaisePascal.SmartHouse.Domain/Lighting/Repository/ITwoLampDevicesRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain.Lighting.Repository
+{
+    internal interface ITwoLampDevicesRepository
+    {
+        void Add(TwoLampDevice twolampdevice);
+        void Update(TwoLampDevice twolampdevice);
+        void Delete(TwoLampDevice twolampdevice);
+        TwoLampDevice GetById(int id);
+        List<TwoLampDevice> GetAll();
+    }
+}
