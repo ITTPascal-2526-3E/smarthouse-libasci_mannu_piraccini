8a462993db970f86464a8f55a11dccc1220c37e6
SamueleLibasci <samuelelibasci2000@gmail.com>
2026-02-24 13:25:21 +0100

    Aggiunta implementazione di SwitchRadiatorOnCommand

--- src/BlaisePascal.SmartHouse.Application/Devices/TemperatureRegolationDevice/Radiator/Commands/SwitchRadiatorOnCommand.cs
+++ src/BlaisePascal.SmartHouse.Application/Devices/TemperatureRegolationDevice/Radiator/Commands/SwitchRadiatorOnCommand.cs
@@ -3,10 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlaisePascal.SmartHouse.Domain.TemperatureRegulation.Repository;
 
-namespace BlaisePascal.SmartHouse.Application.Devices.TemperatureRegolationDevice.Radiator.Commands
+namespace BlaisePascal.SmartHouse.Application.Devices.TemperatureRegulationDevice.Radiator.Commands
 {
     internal class SwitchRadiatorOnCommand
     {
+        private readonly IRadiatorRepository _radiatorRepository;
+
+        public SwitchRadiatorOnCommand(IRadiatorRepository radiatorRepository)
+        {
+            _radiatorRepository = radiatorRepository;
+        }
+
+        public void Execute(int radiatorId)
+        {
+            var radiator = _radiatorRepository.GetById(radiatorId);
+            if (radiator == null)
+            {
+                throw new InvalidOperationException($"Radiator with ID {radiatorId} not found.");
+            }
+            if (radiator.IsOn)
+            {
+                throw new InvalidOperationException($"Radiator with ID {radiatorId} is already on.");
+            }
+
+            radiator.TurnOnOrOff();
+            _radiatorRepository.Update(radiator);
+        }
     }
 }
